245edc9dae66ff7ad5d391919a0353708f9bffa9
Patrick Duffy <patrickduffy202@gmail.com>
2022-11-10 23:11:01 -0600

    updated readme

--- models/index.js
+++ models/index.js
@@ -13,6 +13,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,6 +17,7 @@ router.get('/', (req, res) => {
      },
      {
        model: Tag,
+       attributes: ['tag_name'],
      }
     ]
    })
@@ -32,6 +33,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   Product.findOne({
     attributes: [
+      "id",
       'product_name',
       'category_id',
     ],
@@ -61,23 +63,9 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    tagIds: req.body.tagIds
+    category_id: req.body.category_id
   })
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((dbProduct) => res.status(200).json(dbProduct))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -92,34 +80,7 @@ router.put('/:id', (req, res) => {
       id: req.params.id,
     },
   })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
+    .then((dbProduct) => res.json(dbProduct))
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
